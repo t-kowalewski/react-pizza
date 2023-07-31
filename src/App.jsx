77b5b1e9c4fc22bb6f2a7ba9204514568b71bb38
Tony Kowalewski <tony@kowalewski.dev>
2023-07-31 16:43:29 +0200

    init comm & Header start

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h2>Lets get started!</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,11 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <h2>React Pizza</h2>
+    </header>
+  );
+};
+
+export default Header;

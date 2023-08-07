4acf7ee42e54f4f133f93084ef211868d2381389
Tony Kowalewski <tony@kowalewski.dev>
2023-08-08 00:57:34 +0200

    add HeaderCartButton

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,10 +1,12 @@
+import HeaderCartButton from '../HeaderCartButton/HeaderCartButton';
+
 import classes from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={classes.header}>
       <h2>React Pizza</h2>
-      <button>Cart</button>
+      <HeaderCartButton />
     </header>
   );
 };

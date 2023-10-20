0889244f42b84fb6fd552de25b25a888ef984901
Tony Kowalewski <tony@kowalewski.dev>
2023-10-20 23:48:57 +0200

    comment out boilerplate

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -4,8 +4,8 @@ const CartContext = createContext({
   items: [], // [{id: 'p1', name: "Burger", amount: 1, price: 5}]
   totalAmount: 0, // how much
   // fn to update context
-  addItem: (item) => {},
-  removeItem: (id) => {},
+  // addItem: (item) => {},
+  // removeItem: (id) => {},
 });
 
 export default CartContext;
